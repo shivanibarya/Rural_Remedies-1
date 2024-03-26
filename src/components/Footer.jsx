43920262e6ba89a12e211609697c35fd0b0bab7a
shivanibarya <shivanibarya29@gmail.com>
2024-03-26 20:08:33 +0530

    i have created footer with the help of react js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,36 +8,37 @@ function Footer() {
           <div className="col-md-4 col-sm-6">
             <h3>About Rural-Remedies</h3>
             <p>Rural Remedies is dedicated to providing natural remedies for rural communities.</p>
-            <p>Work With Us</p>
-            <p>Report Fraud</p>
-            <p>Firstaid Kit</p>
-            <p>Contact Us</p>
-            <p>Feedback</p>
+            <ul className="list-unstyled">
+              <li><a href="#">Work With Us</a></li>
+              <li><a href="#">Report Fraud</a></li>
+              <li><a href="#">First Aid Kit</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Feedback</a></li>
+            </ul>
           </div>
           <div className="col-md-4 col-sm-6">
             <h3>Connect With Us</h3>
-            <ul>
-            import facebook from './images/facebook.png'
-            <img src={facebook} height='50' width='50'/>
-            import instagram from './images/instragram.jpeg'
-            <img src={instagram} height='50' width='50'/>
-            import twitter from './images/twitter.png'
-            <img src={twitter} height='50' width='50'/>
+            <ul className="list-unstyled">
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Instagram</a></li>
             </ul>
           </div>
           <div className="col-md-4 col-sm-12">
             <h3>Let Us Help You</h3>
-            <p>Your Account</p>
-            <p>Security</p>
-            <p>Term And Condition</p>
-            <p>Collaboration</p>
+            <ul className="list-unstyled">
+              <li><a href="#">Your Account</a></li>
+              <li><a href="#">Security</a></li>
+              <li><a href="#">Terms And Conditions</a></li>
+              <li><a href="#">Collaboration</a></li>
+            </ul>
             <p>Have questions or need assistance? <a href="#">Contact us</a>.</p>
           </div>
         </div>
         <div className="row">
           <div className="col-md-12">
             <hr />
-            <p>&copy; {new Date().getFullYear()} By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners. © Rural-Remedies™ Ltd. All rights reserved</p>
+            <p className="text-center">&copy; {new Date().getFullYear()} By continuing past this page, you agree to our <a href="#">Terms of Service</a>, <a href="#">Cookie Policy</a>, <a href="#">Privacy Policy</a>, and <a href="#">Content Policies</a>. All trademarks are properties of their respective owners. © Rural-Remedies™ Ltd. All rights reserved</p>
           </div>
         </div>
       </div>
