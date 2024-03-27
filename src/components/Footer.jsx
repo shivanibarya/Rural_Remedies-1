553dd03001e9221af3a8f51448aa8af3f3f9d33b
shivanibarya <shivanibarya29@gmail.com>
2024-03-27 12:39:44 +0530

    i have created footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 col-md-6 col-sm-12">
+    <footer id="footer">
+      <div id="footer-container">
+        <div id="footer-row">
+          <div id="about" className="col-lg-4 col-md-6 col-sm">
             <h3>About Rural-Remedies</h3>
             <p>Rural Remedies is dedicated to providing natural remedies for rural communities.</p>
             <ul className="list-unstyled">
@@ -16,7 +16,7 @@ function Footer() {
               <li><a href="#">Feedback</a></li>
             </ul>
           </div>
-          <div className="col-lg-4 col-md-6 col-sm-12">
+          <div id="connect" className="col-lg-4 col-md-6 col-sm-12">
             <h3>Connect With Us</h3>
             <ul className="list-unstyled">
               <li><a href="#">Facebook</a></li>
@@ -24,7 +24,7 @@ function Footer() {
               <li><a href="#">Instagram</a></li>
             </ul>
           </div>
-          <div className="col-lg-4 col-md-12 col-sm-12">
+          <div id="help" className="col-lg-4 col-md-12 col-sm-12">
             <h3>Let Us Help You</h3>
             <ul className="list-unstyled">
               <li><a href="#">Your Account</a></li>
@@ -35,8 +35,8 @@ function Footer() {
             <p>Have questions or need assistance? <a href="#">Contact us</a>.</p>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-12">
+        <div id="footer-row-bottom" className="row">
+          <div id="footer-bottom" className="col-md-12">
             <hr />
             <p className="text-center">&copy; {new Date().getFullYear()} By continuing past this page, you agree to our <a href="#">Terms of Service</a>, <a href="#">Cookie Policy</a>, <a href="#">Privacy Policy</a>, and <a href="#">Content Policies</a>. All trademarks are properties of their respective owners. © Rural-Remedies™ Ltd. All rights reserved</p>
           </div>
