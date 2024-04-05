23dc808ac751afae59f0781ccb134dc36c70739a
shivanibarya <shivanibarya29@gmail.com>
2024-04-05 19:52:03 +0530

    i have created a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,62 +1,69 @@
 import React from 'react';
-import './footer.css';
-import Facebook from "../assets/FB.png";
-import Insta from "../assets/insta.png";
-import Twit from "../assets/twitter.png";
-function Footer() {
-  return (
-    <footer  id="footer">
-      
-       <div id="footer-row">
 
-          <div id="about" className="col-lg-4 col-md-6 col-sm">
-            <h3>About Rural-Remedies</h3>
-            <p className='text-white'>Rural Remedies is dedicated to providing <br/> natural remedies for rural communities.</p>
-            <ul className="list-unstyled">
-              <li><a href="#">Work With Us</a></li>
-              <li><a href="#">Report Fraud</a></li>
-              <li><a href="#">First Aid Kit</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Feedback</a></li>
-            </ul>
+class Footer extends React.Component {
+  render() {
+    return (
+<footer className="mainfooter" role="contentinfo" style={{ 
+  backgroundImage: `url('../src/assets/o.jpg')`,
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  backgroundBlendMode: 'multiply'
+}}>
+  {/* Content of the footer */}
+
+
+        <div className="footer-middle">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3 col-sm-6">
+                <div className="footer-pad">
+                  <h4 style={{ color: "white" }}>Rural-Remedies</h4>
+                  <img style={{ width: "60px" }} src="..\src\assets\icon 2.jpg" alt="" srcSet="" />
+                  <p style={{ color: "white" }}>"Our rural remedies: Time-tested solutions rooted in tradition for common health needs, promoting natural wellness with simplicity."</p>
+                  <img style={{ width: "40px", margin: 10 }} className="linkedinImg" src="..\src\assets\linkedin_socialnetwork_17441.png" alt="" srcSet="" />
+                  <img style={{ width: "40px", margin: 10}} src="..\src\assets\twitter-icone.png" alt="" srcSet="" />
+                  <img style={{ width: "40px", margin: 10}} src="..\src\assets\FB.png" alt="" srcSet="" />
+
+                </div>
+              </div>
+              <div className="col-md-3 col-sm-6">
+                <div className="footer-pad">
+                  <h4 style={{ color: "white" }}>Popular Remedies</h4>
+                  <img style={{ width: "100px" }} src="..\src\assets\th (2).jpeg" alt="" srcSet="" />
+                  <p style={{ color: "white" }}>Ashwagandha: Stress relief, energy boost, immune support, cognitive enhancement..</p>
+                  <img style={{ width: "100px" }} src="..\src\assets\turmeric.jpg" alt="" srcSet="" />
+                  <p style={{ color: "white" }}>Turmeric: Anti-inflammatory, antioxidant, digestive aid, wound healer, respiratory support.</p>
+                </div>
+              </div>
+              <div className="col-md-3 col-sm-6">
+                <div className="footer-pad">
+                  <h4 style={{ color: "white" }}>Get In Touch</h4>
+                  <img style={{ width: "30px" }} src="..\src\assets\email_PNG100751.PNG" alt="" srcSet="" />
+                  <p style={{ color: "white" }}>mail@gmail.com</p>
+                  <img style={{ width: "30px" }} src="..\src\assets\R (1).png" alt="" srcSet="" />
+                  <p style={{ color: "white" }}>+1-123-456-7890</p>
+                  <img style={{ width: "30px" }} src="..\src\assets\adress-logo-clipart-7.jpg" alt="" srcSet="" />
+                  <p style={{ color: "white" }}>123 Rural-Remedies pure street, city, country</p>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <h4 style={{ color: "white" }}>Subscribe</h4>
+                <p style={{ color: "white" }}>Get our latest remedies, updates, enter your email and subscribe to our newsletter</p>
+                <form>
+                  <input type="email" placeholder="enter your email" />
+                  <span><input type="submit" value="submit" /></span>
+                </form>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 copy">
+                <p style={{ color: "white" }} className="text-center">&copy; Copyright 2024 - Company Name. All rights reserved.</p>
+              </div>
+            </div>
           </div>
-
-          <div id="help" className="col-lg-4 col-md-12 col-sm-12">
-            <h3>Let Us Help You</h3>
-            <ul className="list-unstyled">
-              <li><a href="#">Your Account</a></li>
-              <li><a href="#">Security</a></li>
-              <li><a href="#">Terms And Conditions</a></li>
-              <li><a href="#">Collaboration</a></li>
-            </ul>
-            <p className='text-white'>Have questions or need assistance? <a href="#">Contact us</a>.</p>
-          </div>
-
-          <div id="connect" className="col-lg-4 col-md-6 col-sm-12">
-            <h3>Connect With Us</h3>
-            <ul className="list-unstyled ">
-              <li className='py-2'><img className='social-img' src={Facebook} alt="loading...." /><a href="/">Facebook</a></li>
-              <li className='py-2'><img className='social-img' src={Insta} alt="loading...." /><a href="/">Instagram</a></li>
-              <li className='py-2'><img className='social-img' src={Twit} alt="loading...." /><a href="/">Twitter</a></li>
-             
-            </ul>
-          </div>
-
-
-        
-
-
         </div>
-
-        <div id="footer-row-bottom" className="row">
-          <div id="footer-bottom" className="col-md-12">
-            <hr />
-            <p className="text-center text-white">&copy; {new Date().getFullYear()} By continuing past this page, you agree to our <a href="#">Terms of Service</a>, <a href="#">Cookie Policy</a>, <a href="#">Privacy Policy</a>, and <a href="#">Content Policies</a>. All trademarks are properties of their respective owners. © Rural-Remedies™ Ltd. All rights reserved</p>
-          </div>
-        </div>
-    
-    </footer>
-  );
+      </footer>
+    );
+  }
 }
 
 export default Footer;
